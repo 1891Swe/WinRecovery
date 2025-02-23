e9907d4532f6d669e5156345652a03ce94d81291
1891Swe <simplifymenu@gmail.com>
2025-02-23 18:09:29 +0100

    Update cleveland.js

--- data/brands/cleveland.js
+++ data/brands/cleveland.js
@@ -1,5 +1,3 @@
-// Initialize golfData object if it doesn't exist
-window.golfData = window.golfData || {};
 
 // Cleveland Golf club data
 window.golfData.cleveland = [
@@ -63,5 +61,140 @@ window.golfData.cleveland = [
         description: "Women's Maximum Forgiveness Game Improvement Iron",
         price: "$899.99",
         reviews: "Coming Soon"
+    },
+    // Wedges
+    {
+        brand: "Cleveland Golf",
+        model: "RTZ Tour Satin Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/rtz/rtz-tour-satin-wedge/MRTZTS.html",
+        description: "RTZ Series Tour Satin Finish",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "RTZ Black Satin Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/rtz/rtz-black-satin-wedge/MRTZBKS.html",
+        description: "RTZ Series Black Satin Finish",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "RTZ Tour Rack (Raw) Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/rtz/rtz-tour-rack-%28raw%29-wedge/MRTZTR.html",
+        description: "RTZ Series Tour Rack Raw Finish",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+
+    {
+        brand: "Cleveland Golf",
+        model: "RTX Full Face 2 Tour Satin Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/rtx-full-face-2/rtx-full-face-2-tour-satin-wedge/MRTXFF2-TS.html",
+        description: "Full Face 2 Series Tour Satin Finish",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "RTX Full Face 2 Tour Rack (Raw) Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/rtx-full-face-2/rtx-full-face-2-tour-rack-%28raw%29-wedge/MRTXFF2-TR.html",
+        description: "Full Face 2 Series Tour Rack Raw Finish",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    
+    {
+        brand: "Cleveland Golf",
+        model: "RTX 6 ZipCore Tour Satin Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/rtx-6-zipcore/rtx-6-zipcore-tour-satin-wedge/MRTX6ZCTS.html",
+        description: "RTX 6 ZipCore Tour Satin Finish",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "RTX 6 ZipCore Tour Rack (Raw) Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/rtx-6-zipcore/rtx-6-zipcore-tour-rack-%28raw%29-wedge/MRTX6ZCTR.html",
+        description: "RTX 6 ZipCore Tour Rack Raw Finish",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "RTX 6 ZipCore Black Satin Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/rtx-6-zipcore/rtx-6-zipcore-black-satin-wedge/MRTX6ZCBKS.html",
+        description: "RTX 6 ZipCore Black Satin Finish",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+
+    {
+        brand: "Cleveland Golf",
+        model: "CBX 4 ZipCore Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/cbx-price-drop/cbx-4-zipcore-wedge/MCBX4ZC.html",
+        description: "CBX 4 ZipCore Performance",
+        price: "$149.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's CBX 4 ZipCore Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/wedges/womens-cbx-4-zipcore-wedges/womens-cbx-4-zipcore-wedge/MWCBX4ZC.html",
+        description: "Women's CBX 4 ZipCore Performance",
+        price: "$149.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "CBX Full Face 2 Wedge",
+        type: "Wedges",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/wedges/cbx-price-drop/cbx-full-face-2-wedge/MCBXFF2TS.html",
+        description: "CBX Full Face 2 Performance",
+        price: "$149.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "CBX ZipCore Wedge",
+        type: "Wedges",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/wedges/cbx-zipcore-wedges/cbx-zipcore-wedge/MCBXZC.html",
+        description: "CBX ZipCore Performance",
+        price: "$139.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's CBX ZipCore Wedge",
+        type: "Wedges",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/wedges/cbx-zipcore-wedges/womens-cbx-zipcore-wedge/MWCBXZC.html",
+        description: "Women's CBX ZipCore Performance",
+        price: "$139.99",
+        reviews: "Coming Soon"
     }
 ];
+
