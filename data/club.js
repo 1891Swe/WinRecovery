8f02e001ce21624d21d129d59de23c9e987ef8da
1891Swe <simplifymenu@gmail.com>
2025-02-23 11:54:46 +0100

    Create club.js

---
+++ data/club.js
@@ -0,0 +1,15 @@
+const clubsData = [
+    {
+        brand: "Cobra",
+        type: "Irons",
+        url: "https://www.cobragolf.com/en-eu/collections/golf-clubs-irons",
+        description: "Complete collection of Cobra golf irons"
+    },
+    {
+        brand: "TaylorMade",
+        type: "Wedges",
+        url: "https://www.taylormadegolf.com/wedges/",
+        description: "TaylorMade wedge collection"
+    },
+    // Add more clubs here
+];
