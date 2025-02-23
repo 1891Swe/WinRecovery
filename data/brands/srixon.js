be81d9d4567268655a4a1562fbb8ebc903b1b26b
1891Swe <simplifymenu@gmail.com>
2025-02-23 17:21:30 +0100

    Create srixon.js

---
+++ data/brands/srixon.js
@@ -0,0 +1,47 @@
+// Initialize golfData.srixon if it doesn't exist
+window.golfData = window.golfData || {};
+
+// srixon.js
+window.golfData.srixon = [
+    // Drivers
+    {
+        brand: "Srixon",
+        model: "ZX-i Driver",
+        type: "Drivers",
+        year: "2024",
+        url: "https://eu.dunlopsports.com/srixon/clubs/drivers/zxi-driver/zxi-driver/MZXIDRV.html",
+        description: "ZX-i Performance Driver",
+        price: "$599",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Srixon",
+        model: "ZX-i LS Driver",
+        type: "Drivers",
+        year: "2024",
+        url: "https://eu.dunlopsports.com/srixon/clubs/drivers/zxi-ls-driver/zxi-ls-driver/MZXILSDRV.html",
+        description: "ZX-i Low Spin Driver",
+        price: "$599",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Srixon",
+        model: "ZX-i MAX Driver",
+        type: "Drivers",
+        year: "2024",
+        url: "https://eu.dunlopsports.com/srixon/clubs/drivers/zxi-max-driver/zxi-max-driver/MZXIMAXDRV.html",
+        description: "ZX-i Maximum Forgiveness Driver",
+        price: "$599",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Srixon",
+        model: "Women's ZX-i MAX Driver",
+        type: "Drivers",
+        year: "2024",
+        url: "https://eu.dunlopsports.com/srixon/clubs/drivers/women%27s-zxi-max-driver/women%27s-zxi-max-driver/MWZXIMAXDRV.html",
+        description: "Women's ZX-i Maximum Forgiveness Driver",
+        price: "$599",
+        reviews: "Coming Soon"
+    }
+];
