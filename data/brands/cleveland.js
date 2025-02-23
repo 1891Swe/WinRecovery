2d68955a7f924b10e7d5a83427b000499aab30ec
1891Swe <simplifymenu@gmail.com>
2025-02-23 18:24:00 +0100

    Update cleveland.js

--- data/brands/cleveland.js
+++ data/brands/cleveland.js
@@ -195,6 +195,488 @@ window.golfData.cleveland = [
         description: "Women's CBX ZipCore Performance",
         price: "$139.99",
         reviews: "Coming Soon"
+    },
+    // Putters
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Model 1",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-model-1/MHBSOFT2B-1.html",
+        description: "HB SOFT 2 Black Series Blade Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Model 8C",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-model-8c/MHBSOFT2B-8C.html",
+        description: "HB SOFT 2 Black Series Mid-Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Model 8S",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-model-8s/MHBSOFT2B-8S.html",
+        description: "HB SOFT 2 Black Series Mid-Mallet Slant Neck Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Model 10.5P",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-model-10.5p/MHBSOFT2B-105P.html",
+        description: "HB SOFT 2 Black Series Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Model 11",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-model-11/MHBSOFT2B-11.html",
+        description: "HB SOFT 2 Black Series Large Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Model 11C",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-model-11c/MHBSOFT2B-11C.html",
+        description: "HB SOFT 2 Black Series Large Mallet Center Shaft Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Model 11S",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-model-11s/MHBSOFT2B-11S.html",
+        description: "HB SOFT 2 Black Series Large Mallet Slant Neck Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Model 15",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-model-15/MHBSOFT2B-15.html",
+        description: "HB SOFT 2 Black Series High MOI Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 Black - Retreve",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2-black/hb-soft-2-black-putter-%E2%80%93-retreve/MHBSOFT2B-R.html",
+        description: "HB SOFT 2 Black Series Retreve Model Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Model 1",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-model-1/MHBSOFT2-1.html",
+        description: "HB SOFT 2 Series Blade Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Model 8C",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-model-8c/MHBSOFT2-8C.html",
+        description: "HB SOFT 2 Series Mid-Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Model 8S",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-model-8s/MHBSOFT2-8S.html",
+        description: "HB SOFT 2 Series Mid-Mallet Slant Neck Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Model 10.5",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-model-10.5/MHBSOFT2-105P.html",
+        description: "HB SOFT 2 Series Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Model 11",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-model-11/MHBSOFT2-11.html",
+        description: "HB SOFT 2 Series Large Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Model 11C",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-model-11c/MHBSOFT2-11C.html",
+        description: "HB SOFT 2 Series Large Mallet Center Shaft Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Model 11S",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-model-11s/MHBSOFT2-11S.html",
+        description: "HB SOFT 2 Series Large Mallet Slant Neck Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Model 15",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-model-15/MHBSOFT2-15.html",
+        description: "HB SOFT 2 Series High MOI Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB SOFT 2 - Retreve",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-2/hb-soft-2-putter-%E2%80%93-retreve/MHBSOFT2-R.html",
+        description: "HB SOFT 2 Series Retreve Model Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's HB SOFT 2 - Model 1",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/womens-putters/womens-hb-soft-2-putter-%E2%80%93-model-1/MWHBSOFT2-1.html",
+        description: "Women's HB SOFT 2 Series Blade Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's HB SOFT 2 - Model 11",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/womens-putters/womens-hb-soft-2-putter-%E2%80%93-model-11/MWHBSOFT2-11.html",
+        description: "Women's HB SOFT 2 Series Large Mallet Putter",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's HB SOFT 2 - Retreve",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/womens-putters/womens-hb-soft-2-putter-%E2%80%93-retreve/MWHBSOFT2-R.html",
+        description: "Women's HB SOFT 2 Series Retreve Model",
+        price: "$129.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Frontline Elite 1.0",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/putters/frontline-elite-putters/frontline-elite-1.0-putter/MFE1-23.html",
+        description: "Frontline Elite Blade Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Frontline Elite 8.0",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/putters/frontline-elite-putters/frontline-elite-8.0-putter/MFE8-23.html",
+        description: "Frontline Elite Mid-Mallet Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Frontline Elite Elevado Single Bend",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/putters/frontline-elite-putters/frontline-elite-elevado-single-bend-putter/MFEESB-23.html",
+        description: "Frontline Elite Mallet Single Bend Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Frontline Elite Elevado Slant Neck",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/putters/frontline-elite-putters/frontline-elite-elevado-slant-neck-putter/MFEESN-23.html",
+        description: "Frontline Elite Mallet Slant Neck Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Frontline Elite Cero Single Bend",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/putters/frontline-elite-putters/frontline-elite-cero-single-bend-putter/MFECSB-23.html",
+        description: "Frontline Elite High MOI Single Bend Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Frontline Elite Cero Slant Neck",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/putters/frontline-elite-putters/frontline-elite-cero-slant-neck-putter/MFECSN-23.html",
+        description: "Frontline Elite High MOI Slant Neck Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Frontline Elite Rho Single Bend",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/putters/frontline-elite-putters/frontline-elite-rho-single-bend-putter/MFERSB-23.html",
+        description: "Frontline Elite Rho Single Bend Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Frontline Elite Rho Slant Neck",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/putters/frontline-elite-putters/frontline-elite-rho-slant-neck-putter/MFERSN-23.html",
+        description: "Frontline Elite Rho Slant Neck Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's Frontline Elite Elevado Slant Neck",
+        type: "Putters",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/womens-putters/womens-frontline-elite-elevado-slant-neck-putter/MWFEESN-23.html",
+        description: "Women's Frontline Elite Mallet Slant Neck Putter",
+        price: "$199.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 1",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-1-putter/MHBSM1P.html",
+        description: "HB Soft Milled Blade Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 4",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-4-putter/MHBSM4P.html",
+        description: "HB Soft Milled Mid-Size Blade Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 5",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-5-putter/MHBSM5P.html",
+        description: "HB Soft Milled Half-Mallet Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 8",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-8-putter/MHBSM8P.html",
+        description: "HB Soft Milled Mid-Mallet Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 8P",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-8p-putter/MHBSM8PP.html",
+        description: "HB Soft Milled Mid-Mallet Plumber's Neck Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 10.5S",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-10.5s-putter/MHBSM105SP.html",
+        description: "HB Soft Milled Mallet Slant Neck Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 10.5C",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-10.5c-putter/MHBSM105CP.html",
+        description: "HB Soft Milled Mallet Center Shaft Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 11S",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-11s-putter/MHBSM11SP.html",
+        description: "HB Soft Milled Large Mallet Slant Neck Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 11",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-11-putter/MHBSM11P.html",
+        description: "HB Soft Milled Large Mallet Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HB Soft Milled 14",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/hb-soft-milled/hb-soft-milled-14-putter/MHBSM14P.html",
+        description: "HB Soft Milled High MOI Mallet Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's HB Soft 11S Half Mallet",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/womens-putters/womens-huntington-beach-soft-11s-half-mallet-putter/MWHBS11S.html",
+        description: "Women's HB Soft Half Mallet Slant Neck Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's HB Soft Milled 10.5S",
+        type: "Putters",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/putters/womens-putters/womens-hb-soft-milled-10.5s-putter/MWHBSM105SP.html",
+        description: "Women's HB Soft Milled Mallet Slant Neck Putter",
+        price: "$169.99",
+        reviews: "Coming Soon"
+    },
+    //Drivers
+    {
+        brand: "Cleveland Golf",
+        model: "HiBORE XL Driver",
+        type: "Drivers",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/drivers/hibore-xl-driver/hibore-xl-driver/MHBXLDRV.html",
+        description: "Standard HiBORE XL Performance Driver",
+        price: "$349.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "HiBORE XL Lite Driver",
+        type: "Drivers",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/clubs/drivers/hibore-xl-lite-driver/hibore-xl-lite-driver/MHBXLLTDRV.html",
+        description: "Lightweight HiBORE XL Performance Driver",
+        price: "$349.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's HiBORE XL Lite Driver",
+        type: "Drivers",
+        year: "2024",
+        url: "https://us.dunlopsports.com/cleveland-golf/womens/womens-drivers/womens-hibore-xl-lite-driver/MWHBXLLTDRV.html",
+        description: "Women's Lightweight HiBORE XL Performance Driver",
+        price: "$349.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Launcher XL 2 Driver",
+        type: "Drivers",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/drivers/launcher-xl-2-drivers/launcher-xl-2-drivers/MLXL2D.html",
+        description: "Standard Launcher XL 2 Performance Driver",
+        price: "$299.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Launcher XL 2 Draw Driver",
+        type: "Drivers",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/drivers/launcher-xl-2-draw-drivers/launcher-xl-2-draw-drivers/MLXL2DD.html",
+        description: "Draw-Biased Launcher XL 2 Performance Driver",
+        price: "$299.99",
+        reviews: "Coming Soon"
+    },
+    {
+        brand: "Cleveland Golf",
+        model: "Women's Launcher XL 2 Draw Driver",
+        type: "Drivers",
+        year: "2023",
+        url: "https://us.dunlopsports.com/cleveland-golf/sale/drivers/womens-launcher-xl-2-draw-drivers/womens-launcher-xl-2-draw-drivers/MWLXL2D.html",
+        description: "Women's Draw-Biased Launcher XL 2 Performance Driver",
+        price: "$299.99",
+        reviews: "Coming Soon"
     }
 ];
 
